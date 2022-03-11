4d4b7b880083289285536f30c8e035e3193f6b42
George Stavroulakis <g.stavroulakis@gmail.com>
2022-03-11 13:56:52 +0100

    move state to parent to access from sibling

--- src/components/SearchFlights.jsx
+++ src/components/SearchFlights.jsx
@@ -4,33 +4,57 @@ import DestinationDropdown from "./DestinationDropdown";
 import DepartureDropdown from "./DepartureDropdown";
 import SearchButton from "./SearchButton";
 
-function SearchFlights({fetchFlights}) {
+function SearchFlights({
+	fetchFlights,
+	destination,
+	setDestination,
+	departure,
+	setDeparture,
+}) {
+	//states
+	// const [destination, setDestination] = useState("VLC");
+	// const [departure, setDeparture] = useState("PRG");
+	const [search, setSearch] = useState([departure, destination]);
 
-    //states
-    const [destination, setDestination] = useState("VLC");
-	const [departure, setDeparture] = useState("PRG");
-    const [search, setSearch] = useState([departure, destination]);
+	//useful constants
+	const destinations = [
+		{ name: "Valencia", iata: "VLC" },
+		{ name: "Barcelona", iata: "BCN" },
+		{ name: "Madrid", iata: "MAD" },
+		{ name: "Milan", iata: "MIL" },
+		{ name: "Athens", iata: "ATH" },
+	];
+	const placeOfDeparture = [
+		{ name: "Prague", iata: "PRG" },
+		{ name: "Berlin", iata: "BER" },
+		{ name: "Warsaw", iata: "WAW" },
+		{ name: "Pardubice", iata: "PED" },
+	];
 
-    //useful constants
-    const destinations = [{name : "Valencia", iata : "VLC"}, {name : "Barcelona", iata : "BCN"}, {name : "Madrid", iata : "MAD"}, {name : "Milan", iata : "MIL"}, {name : "Athens", iata : "ATH"}];
-	const placeOfDeparture = [{name : "Prague", iata : "PRG"}, {name : "Berlin", iata : "BER"}, , {name : "Warsaw", iata : "WAW"}, {name : "Pardubice", iata : "PED"}];
+	//saving destination and place of departure into a search variable
+	const handleClick = () => {
+		setSearch([departure, destination]);
+	};
 
-    //saving destination and place of departure into a search variable
-    const handleClick = () => {
-        setSearch([departure, destination]);        
-    }
+	useEffect(() => {
+		fetchFlights(search[0], search[1]);
+	}, [search]);
 
-    useEffect(() => {
-        fetchFlights(search[0], search[1]);
-    }, [search]);
-
-    return (
-        <>
-            <DepartureDropdown placeOfDeparture={placeOfDeparture} departure={departure} setDeparture={setDeparture} /> 
-            <DestinationDropdown destinations={destinations} destination={destination} setDestination={setDestination} />
-            <SearchButton handleClick={handleClick}/>
-        </>
-    );
+	return (
+		<>
+			<DepartureDropdown
+				placeOfDeparture={placeOfDeparture}
+				departure={departure}
+				setDeparture={setDeparture}
+			/>
+			<DestinationDropdown
+				destinations={destinations}
+				destination={destination}
+				setDestination={setDestination}
+			/>
+			<SearchButton handleClick={handleClick} />
+		</>
+	);
 }
 
 export default SearchFlights;
